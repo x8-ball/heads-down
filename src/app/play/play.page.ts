cf9867d858f69977eb57815944d7bae1925a2e85
Thomas <thomes.f@hotmail.de>
2022-05-16 18:19:16 +0200

    extended in lot of ways

--- src/app/play/play.page.ts
+++ src/app/play/play.page.ts
@@ -10,57 +10,69 @@ import { Platform } from '@ionic/angular';
 })
 
 export class PlayPage implements OnInit {
+  
+  spacing = 0.15; 
+
+  correctSoundPath = 'assets/audio/correct.wav';
+  wrongSoundPath = 'assets/audio/wrong.wav';
+  countdownSoundPath = 'assets/audio/countdown.wav';
+
   time_remaining: number = 0;
   time_to_play: number = 0;
   data: any;
 
   interval = null;
   timerSecondsTotal = 0;
+  lastPressedAnswer = Date.now();
+  delta = 0;
 
-  currentWord: Word = { "text": "Ready?"};
-
+  platform;
+  currentWord: string ="Ready?";
+  countdownPlayed: boolean;
   result : any[];
 
-  width : number = 0;
-  height : number = 0;
-
   constructor(private route: ActivatedRoute, private router: Router, private http: HttpClient, platform: Platform) {
+    this.countdownPlayed = false;
     this.route.queryParams.subscribe(params => {
       if (this.router.getCurrentNavigation().extras.state) {
         let dataset = this.router.getCurrentNavigation().extras.state.dataset;
-        this.http.get('../../assets/data/data.json')
+        this.http.get('../../assets/data/'+dataset.name+'.txt',{"responseType":'text'})
         .subscribe(data => {
-          this.data = data;
+          this.data = data.toString().split("\n");
           this.chooseRandomWord();
         });
         this.time_to_play = this.router.getCurrentNavigation().extras.state.timer.value;
       }
       platform.ready().then(() => {
-        this.width = platform.width();
-        this.height = platform.height();
-        //console.log('Width: ' + platform.width());
-        //console.log('Height: ' + platform.height());
+        this.platform = platform;
       });
       this.result = [];
     });
   }
-
+  ngOnInit() {
+    this.chooseRandomWord();
+    //this.runTimer();
+  }
   chooseRandomWord(){
     if(!this.data) return;
     if(this.data.length == 0) this.showResult();
     var index = Math.trunc(Math.random()*this.data.length);
     this.currentWord = this.data[index];
     this.data.splice(index,1);
-    //console.log(index,this.currentWord,this.data);
   }
+
   runTimer() {
     var prevTickTimestamp = Date.now()
     if(this.interval) clearInterval(this.interval);
     this.interval = setInterval(() => {
       var currentTickTimestamp = Date.now()
-      var delta = currentTickTimestamp - prevTickTimestamp
+      this.delta = currentTickTimestamp - prevTickTimestamp;
 
-      this.timerSecondsTotal = Math.round(delta / 1000)
+      this.timerSecondsTotal = Math.round(this.delta / 1000);
+      if(this.time_to_play - this.timerSecondsTotal <= 3 && !this.countdownPlayed){
+        this.countdownPlayed = true;
+        this.playSound(this.countdownSoundPath);
+      }
 
       if(this.time_to_play - this.timerSecondsTotal < 0){
         this.showResult();
@@ -70,7 +82,6 @@ export class PlayPage implements OnInit {
   }
   showResult(){
     clearInterval(this.interval);
-    //console.log(this.result);
     let navigationExtras: NavigationExtras = {
       state: {
         result: this.result
@@ -78,38 +89,58 @@ export class PlayPage implements OnInit {
     };
     this.router.navigate(['result'], navigationExtras);
   }
-  ngOnInit() {
-    this.chooseRandomWord();
-    //this.runTimer();
+  getWidth(){
+    return this.platform.width();
   }
-
+  getHeight(){
+    return this.platform.height();
+  }
+  
   getCoordinates(event){
-    // This output's the X coord of the click
-    //console.log(event.clientX);
-    if(event.clientX < this.width / 2 - this.width*0.2){
+    if(!this.toggleCheck()) return;
+    if(this.isLeft(event.clientX)){
+      this.correct();
+    }
+    else if(this.isRight(event.clientX)){
       this.wrong();
     }
-    else if(event.clientX > this.width / 2 + this.width*0.2){
-      this.right()
-    }
   }
 
-  wrong(){
-    //console.log("wrong")
-    let answer = {
-      "word": this.currentWord,
-      "status" : "wrong"
-    };
-    this.result.push(answer);
-    this.chooseRandomWord();
+  toggleCheck(){
+    var currentTickTimestamp = Date.now()
+    var delta = currentTickTimestamp - this.lastPressedAnswer;
+    if(delta >= 500){
+      this.lastPressedAnswer = currentTickTimestamp;
+      return true;
+    }
+    else {
+      return false;
+    }
   }
-  right(){
-    //console.log("right")
-    let answer = {
+  isLeft(posX){
+    return posX >= this.getWidth()*this.spacing && posX < this.getWidth() / 2
+  }
+  isRight(posX){
+    return posX > this.getWidth() / 2 && posX <= this.getWidth() * (1 - this.spacing);
+  }
+  async playSound(path){
+    let sound = new Audio(path);
+    sound.load();
+    await sound.play();
+  }
+  wrong(){
+    this.playSound(this.wrongSoundPath);
+    this.addWord("wrong");
+  }
+  correct(){
+    this.playSound(this.correctSoundPath);
+    this.addWord("correct");
+  }
+  addWord(status){
+    this.result.push({
       "word": this.currentWord,
-      "status" : "right"
-    };
-    this.result.push(answer);
+      "status" : status
+    });
     this.chooseRandomWord();
   }
 };
